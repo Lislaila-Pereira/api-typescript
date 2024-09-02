1e1337ba66573975223b8ea79837f2ee097ffef1
Lislaila Pereira <lislailapereira@gmail.com>
2024-09-02 20:16:08 -0300

    criando e configurando rota para listar clientes

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,22 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
 import { CreateCustomerController } from "./controllers/CreateCustomerController";
+import { ListCustomersController } from "./controllers/ListCustomersController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
 
+    //rota teste
     fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
         return { ok: true };
     })
+
+    //cadastrar cliente
     fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
         return new CreateCustomerController().handle(request, reply);
     })
+
+    //listar clientes
+    fastify.get("/getcustomers", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new ListCustomersController().handle(request, reply);
+    })
+
 }
