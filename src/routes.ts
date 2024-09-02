05bf3234f7289fc7b9185c46c2ab23a4f089dd4c
Lislaila Pereira <lislailapereira@gmail.com>
2024-09-02 20:38:57 -0300

    configura rota para deletar cliente

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
 import { CreateCustomerController } from "./controllers/CreateCustomerController";
 import { ListCustomersController } from "./controllers/ListCustomersController";
+import { DeleteCustomerController } from "./controllers/DeleteCustomerController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
 
@@ -18,5 +19,10 @@ export async function routes(fastify: FastifyInstance, options: FastifyPluginOpt
     fastify.get("/getcustomers", async (request: FastifyRequest, reply: FastifyReply) => {
         return new ListCustomersController().handle(request, reply);
     })
+
+    //deletar cliente
+    fastify.delete("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
+            return new DeleteCustomerController().handle(request, reply);
+        })
 
 }
